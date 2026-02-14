88d721d5afd269739a5b44649ca6d7f6b6a72390
Aditya Mishra <adimis.ai.001@gmail.com>
2026-02-14 09:37:06 +0000

    feat: introduce subagents middleware and apply minor formatting to existing test and skills files.

--- src/middlewares/__init__.py
+++ src/middlewares/__init__.py
@@ -1,9 +1,8 @@
-# from .team_runtime import TeamRuntimeMiddleware
-# from .skill_generator import SkillGeneratorMiddleware
 from .skills import SkillsMiddleware
+from .subagents import SubAgentMiddleware, SubAgent
 
 __all__ = [
-    # "TeamRuntimeMiddleware",
-    # "SkillGeneratorMiddleware",
+    "SubAgent",
     "SkillsMiddleware",
+    "SubAgentMiddleware",
 ]
